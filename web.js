bf9e5bf89a46b3be785cf7197651f4df7b2fb324
rogdawg <rogdawg@gmail.com>
2013-07-09 02:10:58 +0000

    corrected type-o in web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.hml');
+  var buffer = fs.readFileSync('index.html');
   response.send(buffer.ToString());
   //response.send('Hello World 2!');
 });
